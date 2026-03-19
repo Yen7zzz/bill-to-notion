1e37e58d8b6e4326ff6a91a652e60c070781643c
Yen7zzz <jason071491@gmail.com>
2026-03-19 16:54:12 +0800

    add cathay in __init__.py

--- src/parsers/__init__.py
+++ src/parsers/__init__.py
@@ -1,10 +1,12 @@
 from .base import BaseParser
 from .esun import ESunParser
 from .sinopac import SinoPacParser
+from .cathay import CathayParser
 
 SENDER_MAP: dict[str, type[BaseParser]] = {
     "estatement@esunbank.com":                  ESunParser,
     "ebillservice@newebill.banksinopac.com.tw": SinoPacParser,
+    "service@pxbillrc01.cathaybk.com.tw":       CathayParser,
 }
 
 def get_parser(sender: str) -> BaseParser | None:
